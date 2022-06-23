83684e7c19e10977e1a792bbb34fcd7dac990969
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-06-23 19:44:04 +0100

    update

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -14,17 +14,29 @@ contract lottery is VRFConsumerBaseV2 {
     address payable[] private s_players;
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     bytes32 private immutable i_gaslane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint16 private constant  NUM_WORDS = 1;
+
+    // Lottery variables
+    address private s_recentWinner;
 
     event newPlayer(uint indexed _amount, address indexed _player);
+    event requestRandomWinner(uint indexed requestId);
 
     constructor(
         address vrfCoordinatorV2,
         uint _entranceFee,
-        bytes32 gaslane
+        bytes32 gaslane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = _entranceFee;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gaslane = gaslane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterLottery() public payable {
@@ -34,13 +46,25 @@ contract lottery is VRFConsumerBaseV2 {
     }
 
     function createRandomWinner() external {
-        
+        uint requestId = i_vrfCoordinator.requestRandomWords(
+            i_gaslane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+
+        emit requestRandomWinner(requestId);
     }
 
     function fulfillRandomWords(uint requestId, uint[] memory randomWords)
         internal
         override
-    {}
+    {
+        uint indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_recentWinner = winner;
+    }
 
     // function withdrawPrice(){}
 
@@ -51,5 +75,9 @@ contract lottery is VRFConsumerBaseV2 {
 
     function getPlayer(uint index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
