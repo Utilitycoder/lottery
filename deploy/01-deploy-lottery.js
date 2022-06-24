d95b2aae1bf73620549dcfec08035bff21979d39
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-06-24 16:45:53 +0100

    updated script

--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -1,14 +1,27 @@
 const { network } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
 module.exports = async ({getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const {deployer} = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let VRFCoordinatorV2Address, subscriptionId
 
     if(developmentChains.includes(network.name)) {
-        
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        VRFCoordinatorV2Address = VRFCoordinatorV2Mock.address
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.event[0].args.subId
+        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        VRFCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
     }
 
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const args =[VRFCoordinatorV2Address, entranceFee, gasLane]
     const lottery = await deploy("Lottery", {
         from: deployer,
         args: [],
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,6 +1,10 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
     31337: {
         name: "localhost",
+        entranceFee: ethers.utils.parseEther("0.1"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc"
     },
     // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
     // Default one is ETH/USD contract on Kovan
@@ -11,6 +15,8 @@ const networkConfig = {
     4: {
         name: "rinkeby",
         vrfCoordinatorV2: "0x6168499c0cffcacd319c818142124b7a15e857ab",
+        entranceFee: ethers.utils.parseEther("0.1"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc"
     },
 }
 
